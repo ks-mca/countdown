3039e200cbf1a4a7a0551fb0851884e80531e086
McaKeith <kschafer@mcaaa.org>
2021-05-27 12:12:34 -0400

    added function to hide HTML by tag

--- countdownConstants.js
+++ countdownConstants.js
@@ -12,7 +12,7 @@ function sessionToFill (sessionToSetup){
 //set Html Templates for countdown clocks
 function FillPage (h2Tag, dayTitle, sessionTitle, sessionSpeaker, counterTag, dayTag, hourTag, minuteTag, secondsTag){
     var temp = document.querySelector('#template');
-    temp.innerHTML+="<div class='container'><div class='countdownBox'><h2 id="+h2Tag+">"+dayTitle+"<br>"+sessionTitle+"<br>"+sessionSpeaker+"<br>Will Begin In:</h2><div id="+counterTag+" class='countdown'><div id="+dayTag+" class='day'></div><div id="+hourTag+" class='hour'></div><div id="+minuteTag+" class='minute'></div><div id="+secondsTag+"  class='second'></div></div></div>";
+    temp.innerHTML+="<div class='container sessionCountdown'><div class='countdownBox'><h2 id="+h2Tag+">"+dayTitle+"<br>"+sessionTitle+"<br>"+sessionSpeaker+"<br>Will Begin In:</h2><div id="+counterTag+" class='countdown'><div id="+dayTag+" class='day'></div><div id="+hourTag+" class='hour'></div><div id="+minuteTag+" class='minute'></div><div id="+secondsTag+"  class='second'></div></div></div>";
 }
 
 //-----------------Countdown Calcs----------------------------
@@ -62,6 +62,6 @@ function sessionInProgress(timerTag, msgDisplayTag, sessionLink){
     document.querySelector("#"+timerTag).style.display="none";
     document.querySelector("#"+msgDisplayTag).innerHTML = "Live Session in Progress<br><a class='sessionLink' href='" +sessionLink+ "' target='_blank'>Click here to join</a>";
 }
-function HideJSMessage(){
-    document.querySelector('.JSNote').style.display="none";
+function HideField(tag){
+    document.querySelector(tag).style.display="none";
 }
