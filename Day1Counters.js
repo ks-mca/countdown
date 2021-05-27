ba4d4766ecbe928b8c4a2b0238a393f158519059
McaKeith <kschafer@mcaaa.org>
2021-05-27 10:48:49 -0400

    Cleaned up Day 1 js code and added msg for js

--- Day1Counters.js
+++ Day1Counters.js
@@ -1,33 +1,27 @@
-
+HideJSMessage();
 //---------------------Day 1-------------------------------------------
-var day1Timer = {
-    s1Start:new Date("August 10, 2021 11:00:00").getTime(),
-    s1End:new Date("August 10, 2021 11:45:00").getTime(),
+var dayTimer = {
+    session1StartTime:new Date("August 10, 2021 11:00:00").getTime(),
+    session1EndTime:new Date("August 10, 2021 11:45:00").getTime(),
     s1StopTimer:0,
-    s2Start:new Date("August 10, 2021 12:15:00").getTime(),
-    s2End:new Date("August 10, 2021 13:15:00").getTime(),
+    session2StartTime:new Date("August 10, 2021 12:15:00").getTime(),
+    session2EndTime:new Date("August 10, 2021 13:15:00").getTime(),
     s2StopTimer:0,
-    s3Start:new Date("August 10, 2021 13:30:00").getTime(),
-    s3End:new Date("August 10, 2021 14:30:00").getTime(),
+    session3StartTime:new Date("August 10, 2021 13:30:00").getTime(),
+    session3EndTime:new Date("August 10, 2021 14:30:00").getTime(),
     s3StopTimer:0
 }
 const dayTitle = "MCA Virtual Summer Conference Day 1"
 //-------------Day 1 Sessions-------------------------------------------
 //------------Opening Speaker-------------------------------------------
-var sessionD1Track1Session1 = {
-    sessionTimeStart: day1Timer.s1Start,
-    sessionTimeEnd: day1Timer.s1End,
-    dTs:'d1t1s1',
+var Session1Opening = {
+    sessionTimeStart: dayTimer.session1StartTime,
+    sessionTimeEnd: dayTimer.session1EndTime,
+    dTs:'Session1',
     dayTitle: dayTitle,
     sessionName:"Keynote Speach",
     sessionPresenter:"Opening Speaker",
     zoomLink: 'http://www.mcaaa.org',
-    countdownTag:'',
-    msgTag:'',
-    dayId: '',
-    hourId: '',
-    minuteId: '',
-    secondsId: '',
     stopTimer:0,
     setTags (){
         this.countdownTag=this.dTs+'Countdown';
@@ -37,24 +31,19 @@ var sessionD1Track1Session1 = {
         this.minuteId= this.dTs+'minute';
         this.secondsId= this.dTs+'second';}
      }     
-sessionD1Track1Session1.setTags();
-sessionToFill(sessionD1Track1Session1);
+Session1Opening.setTags();
+console.log(Session1Opening)
+sessionToFill(Session1Opening);
 //-------------------------------------------------------------------------
 //---------------- Session 2 Track 1 --------------------------------------
-var sessionD1Track1Session2 = {
-    sessionTimeStart: day1Timer.s2Start,
-    sessionTimeEnd: day1Timer.s2End,
-    dTs:'d1t1s2',
+var session2Track1 = {
+    sessionTimeStart: dayTimer.session2StartTime,
+    sessionTimeEnd: dayTimer.session2EndTime,
+    dTs:'Session2Track1',
     dayTitle: dayTitle,
     sessionName:"Session Name here",
     sessionPresenter:"Presenter Name here",
     zoomLink: '#',
-    countdownTag:'',
-    msgTag:'',
-    dayId: '',
-    hourId: '',
-    minuteId: '',
-    secondsId:'',
     stopTimer:0,
     setTags (){
         this.countdownTag=this.dTs+'Countdown';
@@ -64,24 +53,18 @@ var sessionD1Track1Session2 = {
         this.minuteId= this.dTs+'minute';
         this.secondsId= this.dTs+'second';}
      }     
-sessionD1Track1Session2.setTags();
-sessionToFill(sessionD1Track1Session2);
+session2Track1.setTags();
+sessionToFill(session2Track1);
 //-------------------------------------------------------------------------
 //---------------- Session 2 Track 2 --------------------------------------
-var sessionD1Track2Session2 = {
-    sessionTimeStart: day1Timer.s2Start,
-    sessionTimeEnd: day1Timer.s2End,
-    dTs:'d1t2s2',
+var session2Track2 = {
+    sessionTimeStart: dayTimer.session2StartTime,
+    sessionTimeEnd: dayTimer.session2EndTime,
+    dTs:'Session2Track2',
     dayTitle: dayTitle,
     sessionName:"Session Name here",
     sessionPresenter:"Presenter Name here",
     zoomLink: '#',
-    countdownTag:'',
-    msgTag:'',
-    dayId: '',
-    hourId: '',
-    minuteId: '',
-    secondsId:'',
     stopTimer:0,
     setTags (){
         this.countdownTag=this.dTs+'Countdown';
@@ -91,24 +74,18 @@ var sessionD1Track2Session2 = {
         this.minuteId= this.dTs+'minute';
         this.secondsId= this.dTs+'second';}
      }     
-sessionD1Track2Session2.setTags();
-sessionToFill(sessionD1Track2Session2);
+session2Track2.setTags();
+sessionToFill(session2Track2);
 //-------------------------------------------------------------------------
 //---------------- Session 2 Track 3 --------------------------------------
-var sessionD1Track3Session2 = {
-    sessionTimeStart: day1Timer.s2Start,
-    sessionTimeEnd: day1Timer.s2End,
-    dTs:'d1t3s2',
+var session2Track3 = {
+    sessionTimeStart: dayTimer.session2StartTime,
+    sessionTimeEnd: dayTimer.session2EndTime,
+    dTs:'Session2Track3',
     dayTitle: dayTitle,
     sessionName:"Session Name here",
     sessionPresenter:"Presenter Name here",
     zoomLink: '#',
-    countdownTag:'',
-    msgTag:'',
-    dayId: '',
-    hourId: '',
-    minuteId: '',
-    secondsId:'',
     stopTimer:0,
     setTags (){
         this.countdownTag=this.dTs+'Countdown';
@@ -118,24 +95,18 @@ var sessionD1Track3Session2 = {
         this.minuteId= this.dTs+'minute';
         this.secondsId= this.dTs+'second';}
      }     
-sessionD1Track3Session2.setTags();
-sessionToFill(sessionD1Track3Session2);
+session2Track3.setTags();
+sessionToFill(session2Track3);
 //-------------------------------------------------------------------------
 //---------------- Session 3 Track 1 --------------------------------------
-var sessionD1Track1Session3 = {
-    sessionTimeStart: day1Timer.s3Start,
-    sessionTimeEnd: day1Timer.s3End,
-    dTs:'d1t1s3',
+var session3Track1 = {
+    sessionTimeStart: dayTimer.session3StartTime,
+    sessionTimeEnd: dayTimer.session3EndTime,
+    dTs:'Session3Track1',
     dayTitle: dayTitle,
     sessionName:"Session Name here",
     sessionPresenter:"Presenter Name here",
     zoomLink: '#',
-    countdownTag:'',
-    msgTag:'',
-    dayId: '',
-    hourId: '',
-    minuteId: '',
-    secondsId:'',
     stopTimer:0,
     setTags (){
         this.countdownTag=this.dTs+'Countdown';
@@ -145,24 +116,18 @@ var sessionD1Track1Session3 = {
         this.minuteId= this.dTs+'minute';
         this.secondsId= this.dTs+'second';}
      }     
-sessionD1Track1Session3.setTags();
-sessionToFill(sessionD1Track1Session3);
+session3Track1.setTags();
+sessionToFill(session3Track1);
 //-------------------------------------------------------------------------
 //---------------- Session 3 Track 2 --------------------------------------
-var sessionD1Track2Session3 = {
-    sessionTimeStart: day1Timer.s3Start,
-    sessionTimeEnd: day1Timer.s3End,
-    dTs:'d1t2s3',
+var session3Track2 = {
+    sessionTimeStart: dayTimer.session3StartTime,
+    sessionTimeEnd: dayTimer.session3EndTime,
+    dTs:'Session3Track2',
     dayTitle: dayTitle,
     sessionName:"Session Name here",
     sessionPresenter:"Presenter Name here",
     zoomLink: '#',
-    countdownTag:'',
-    msgTag:'',
-    dayId: '',
-    hourId: '',
-    minuteId: '',
-    secondsId:'',
     stopTimer:0,
     setTags (){
         this.countdownTag=this.dTs+'Countdown';
@@ -172,24 +137,18 @@ var sessionD1Track2Session3 = {
         this.minuteId= this.dTs+'minute';
         this.secondsId= this.dTs+'second';}
      }     
-sessionD1Track2Session3.setTags();
-sessionToFill(sessionD1Track2Session3);
+session3Track2.setTags();
+sessionToFill(session3Track2);
 //-------------------------------------------------------------------------
 //---------------- Session 3 Track 3 --------------------------------------
-var sessionD1Track3Session3 = {
-    sessionTimeStart: day1Timer.s3Start,
-    sessionTimeEnd: day1Timer.s3End,
-    dTs:'d1t3s3',
+var session3Track3 = {
+    sessionTimeStart: dayTimer.session3StartTime,
+    sessionTimeEnd: dayTimer.session3EndTime,
+    dTs:'Session3Track3',
     dayTitle: dayTitle,
     sessionName:"Session Name here",
     sessionPresenter:"Presenter Name here",
     zoomLink: '#',
-    countdownTag:'',
-    msgTag:'',
-    dayId: '',
-    hourId: '',
-    minuteId: '',
-    secondsId:'',
     stopTimer:0,
     setTags (){
         this.countdownTag=this.dTs+'Countdown';
@@ -199,26 +158,9 @@ var sessionD1Track3Session3 = {
         this.minuteId= this.dTs+'minute';
         this.secondsId= this.dTs+'second';}
      }     
-sessionD1Track3Session3.setTags();
-sessionToFill(sessionD1Track3Session3);
+session3Track3.setTags();
+sessionToFill(session3Track3);
 //-------------------------------------------------------------------------
-
-//Constant Calcs
-const second = 1000
-const minute = second *60
-const hour = minute *60
-const day = hour *24
-
-//assign session values to pass to HTML template
-function sessionToFill (sessionToSetup){
-    FillPage(sessionToSetup.msgTag, sessionToSetup.dayTitle, sessionToSetup.sessionName, sessionToSetup.sessionPresenter, sessionToSetup.countdownTag, sessionToSetup.dayId, sessionToSetup.hourId, sessionToSetup.minuteId, sessionToSetup.secondsId);
-}
-
-//set Html Templates for countdown clocks
-function FillPage (h2Tag, dayTitle, sessionTitle, sessionSpeaker, counterTag, dayTag, hourTag, minuteTag, secondsTag){
-    var temp = document.querySelector('#template');
-    temp.innerHTML+="<div class='container'><div class='countdownBox'><h2 id="+h2Tag+">"+dayTitle+"<br>"+sessionTitle+"<br>"+sessionSpeaker+"<br>Will Begin In:</h2><div id="+counterTag+" class='countdown'><div id="+dayTag+" class='day'></div><div id="+hourTag+" class='hour'></div><div id="+minuteTag+" class='minute'></div><div id="+secondsTag+"  class='second'></div></div></div>";
-}
 
 setInterval(function(){
     var currentTime = new Date().getTime();
@@ -226,62 +168,14 @@ setInterval(function(){
     // var currentTime = new Date(2021,07,10,12,1).getTime(); //for testing 
 
     //Opening Speaker
-    updateCountdowns(currentTime,sessionD1Track1Session1), 1000;
+    updateCountdowns(currentTime,Session1Opening), 1000;
 
     //Session 2
-    updateCountdowns(currentTime,sessionD1Track1Session2), 1000;
-    updateCountdowns(currentTime,sessionD1Track2Session2), 1000;
-    updateCountdowns(currentTime,sessionD1Track3Session2), 1000;
+    updateCountdowns(currentTime,session2Track1), 1000;
+    updateCountdowns(currentTime,session2Track2), 1000;
+    updateCountdowns(currentTime,session2Track3), 1000;
     //Session 3
-    updateCountdowns(currentTime,sessionD1Track1Session3), 1000;
-    updateCountdowns(currentTime,sessionD1Track2Session3), 1000;
-    updateCountdowns(currentTime,sessionD1Track3Session3), 1000;
+    updateCountdowns(currentTime,session3Track1), 1000;
+    updateCountdowns(currentTime,session3Track2), 1000;
+    updateCountdowns(currentTime,session3Track3), 1000;
 })
-
-//-----------------Countdown Calcs----------------------------
-function endDay(sessionTimeSlot, now){
-    gap = sessionTimeSlot-now;
-    const calcDay = Math.floor(gap /(day));
-    return calcDay;
-}
-function endHour(sessionTimeSlot, now){
-    gap = sessionTimeSlot-now;
-    const calcHour = Math.floor((gap % (day)) / (hour));
-    return calcHour;
-}
-function endMinute(sessionTimeSlot, now){
-    gap = sessionTimeSlot-now;
-    const calcMin = Math.floor((gap % (hour)) / (minute));
-    return calcMin;
-}
-function endSecond(sessionTimeSlot, now){
-    gap = sessionTimeSlot-now;
-    const calcSec = Math.floor((gap % (minute)) / (second));
-    return calcSec;
-}
-//------------------------------------------------------------
-//Update HTML with current countdown values
-function updateCountdowns(time, session){
-    if((time>session.sessionTimeStart)&&(time<session.sessionTimeEnd)&&(session.stopTimer==0)){
-        sessionInProgress(session.countdownTag, session.msgTag, session.zoomLink);
-        session.stopTimer=1;
-    }
-    else if((time>session.sessionTimeEnd)&&(session.stopTimer==1)){
-        sessionEnded(session.countdownTag, session.msgTag);
-        session.stopTimer=2;
-    }
-
-    document.querySelector("#"+session.dayId).innerText = endDay(session.sessionTimeStart, time);
-    document.querySelector("#"+session.hourId).innerText = endHour(session.sessionTimeStart, time);
-    document.querySelector("#"+session.minuteId).innerText = endMinute(session.sessionTimeStart, time);
-    document.querySelector("#"+session.secondsId).innerText = endSecond(session.sessionTimeStart, time);
-}
-function sessionEnded(timerTag, msgDisplayTag){
-    document.querySelector("#"+timerTag).style.display="none";
-    document.querySelector("#"+msgDisplayTag).innerHTML = "Live session has ended<br>The replay should be available soon";
-
-}
-function sessionInProgress(timerTag, msgDisplayTag, sessionLink){
-    document.querySelector("#"+timerTag).style.display="none";
-    document.querySelector("#"+msgDisplayTag).innerHTML = "Live Session in Progress<br><a class='sessionLink' href='" +sessionLink+ "' target='_blank'>Click here to join</a>";
-}
