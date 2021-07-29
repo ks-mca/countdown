0900859ca44d3550b71e7546d68112030f5807a9
McaKeith <kschafer@mcaaa.org>
2021-07-29 11:48:36 -0400

    Moved script to LMSTimer

--- LMSTimer.js
+++ LMSTimer.js
@@ -51,15 +51,16 @@ var day3Timer = {
 }
 //------------------------------------------------------------------------
 //------------Opening Speaker-------------------------------------------
-var Session1Opening = {
+var KeynoteSpeach = {
     sessionTimeStart: day1Timer.session1StartTime,
     sessionTimeEnd: day1Timer.session1EndTime,
-    dTs: 'Session1',
+    dTs: 'Keynote',
     dayTitle: dayTitle,
-    sessionName: "Keynote Speach",
-    sessionPresenter: "Opening Speaker",
+    sessionName: "Welcome to the 2021 MCA Virtual Conference",
+    sessionPresenter: "Join us for a Keynote Speach from Dr. Mona Hanna-Attisha",
     zoomLink: 'http://www.mcaaa.org', //must include the http:// or https:// in order to work
     stopTimer: 0,
+    bannerImgUrl: "https://mcac.mclms.net/en/content-library/access/183843",
     setTags() {
         this.countdownTag = this.dTs + 'Countdown';
         this.msgTag = this.dTs + 'CountMsg';
@@ -69,8 +70,12 @@ var Session1Opening = {
         this.secondsId = this.dTs + 'second';
     }
 }
-Session1Opening.setTags();
-LMSToFill(Session1Opening);
+KeynoteSpeach.setTags();
+
+//-------------------------------------------------------------------------
+//Set the intervals for each session banner to update
+
+LMSToFill(KeynoteSpeach);
 //-------------------------------------------------------------------------
 //Set the intervals for each session to update
 setInterval(function(){
@@ -79,5 +84,144 @@ setInterval(function(){
     //currentTime = newTime; //for testing
 
     //Opening Speaker
-    updateLMSCountdowns(currentTime,Session1Opening), 1000;
+    updateLMSCountdowns(currentTime,KeynoteSpeach), 1000;
 })
+
+//-------------------------------------------------------------------------
+//Set the banner functions
+var bannerStatus = 1;
+var bannerTimer = 6000;
+var startBannerLoop;
+var bannerType;
+
+    function startLoop(){
+        startBannerLoop = setInterval(() => {
+            bannerLoop(); 
+        }, bannerTimer);
+    }
+    function banType(BanType){
+        bannerType=BanType;
+        console.log(bannerType);
+    }
+    document.getElementById("main-banner").onmouseenter = function(){
+        stopLoop();
+    }
+    document.getElementById("main-banner").onmouseleave = function(){
+        if(bannerType=="Multi"){
+            startLoop();
+        }
+        else{}
+    }
+    
+    document.getElementById("imgbanbtn-next").onclick = function(){
+        bannerLoop();
+    }
+    document.getElementById("imgbanbtn-prev").onclick = function(){
+        if(bannerStatus === 1){
+            bannerStatus=2;
+        }
+        else if (bannerStatus === 2) {
+            bannerStatus=3;
+        }
+        else if (bannerStatus === 3) {
+            bannerStatus=1;
+        }
+        bannerLoopReverse();
+    }
+    function stopLoop(){
+        clearInterval(startBannerLoop);
+    }
+    function bannerLoop(){
+        if (bannerStatus ===1) {
+            document.getElementById("imgban2").style.opacity = "0";
+            setTimeout(() => {
+                document.getElementById("imgban1").style.right = "0em";
+                document.getElementById("imgban1").style.zIndex = "1000";
+                document.getElementById("imgban2").style.right = "-80em";
+                document.getElementById("imgban2").style.zIndex = "1500";
+                document.getElementById("imgban3").style.right = "80em";
+                document.getElementById("imgban3").style.zIndex = "500";  
+            }, 500);
+            setTimeout(() => {
+                document.getElementById("imgban2").style.opacity = "1";    
+            }, 1000);
+            bannerStatus = 2;
+        }
+        else if (bannerStatus ===2) {
+            document.getElementById("imgban3").style.opacity = "0";
+            setTimeout(() => {
+                document.getElementById("imgban2").style.right = "0em";
+                document.getElementById("imgban2").style.zIndex = "1000";
+                document.getElementById("imgban3").style.right = "-80em";
+                document.getElementById("imgban3").style.zIndex = "1500";
+                document.getElementById("imgban1").style.right = "80em";
+                document.getElementById("imgban1").style.zIndex = "500";  
+            }, 500);
+            setTimeout(() => {
+                document.getElementById("imgban3").style.opacity = "1";    
+            }, 1000);
+            bannerStatus = 3;
+        }
+        else if (bannerStatus ===3) {
+            document.getElementById("imgban1").style.opacity = "0";
+            setTimeout(() => {
+                document.getElementById("imgban3").style.right = "0em";
+                document.getElementById("imgban3").style.zIndex = "1000";
+                document.getElementById("imgban1").style.right = "-80em";
+                document.getElementById("imgban1").style.zIndex = "1500";
+                document.getElementById("imgban2").style.right = "80em";
+                document.getElementById("imgban2").style.zIndex = "500";  
+            }, 500);
+            setTimeout(() => {
+                document.getElementById("imgban1").style.opacity = "1";    
+            }, 1000);
+            bannerStatus = 1;
+        }
+    }
+    function bannerLoopReverse(){
+        if (bannerStatus ===1) {
+            document.getElementById("imgban3").style.opacity = "0";
+            setTimeout(() => {
+                document.getElementById("imgban1").style.right = "0em";
+                document.getElementById("imgban1").style.zIndex = "1000";
+                document.getElementById("imgban3").style.right = "80em";
+                document.getElementById("imgban3").style.zIndex = "1500";
+                document.getElementById("imgban2").style.right = "-80em";
+                document.getElementById("imgban2").style.zIndex = "500";  
+            }, 500);
+            setTimeout(() => {
+                document.getElementById("imgban3").style.opacity = "1";    
+            }, 1000);
+            bannerStatus = 2;
+        }
+        else if (bannerStatus ===2) {
+            document.getElementById("imgban1").style.opacity = "0";
+            setTimeout(() => {
+                document.getElementById("imgban2").style.right = "0em";
+                document.getElementById("imgban2").style.zIndex = "1000";
+                document.getElementById("imgban1").style.right = "80em";
+                document.getElementById("imgban1").style.zIndex = "1500";
+                document.getElementById("imgban3").style.right = "-80em";
+                document.getElementById("imgban3").style.zIndex = "500";  
+            }, 500);
+            setTimeout(() => {
+                document.getElementById("imgban1").style.opacity = "1";    
+            }, 1000);
+            bannerStatus = 3;
+        }
+        else if (bannerStatus ===3) {
+            document.getElementById("imgban2").style.opacity = "0";
+            setTimeout(() => {
+                document.getElementById("imgban3").style.right = "0em";
+                document.getElementById("imgban3").style.zIndex = "1000";
+                document.getElementById("imgban2").style.right = "80em";
+                document.getElementById("imgban2").style.zIndex = "1500";
+                document.getElementById("imgban1").style.right = "-80em";
+                document.getElementById("imgban1").style.zIndex = "500";  
+            }, 500);
+            setTimeout(() => {
+                document.getElementById("imgban2").style.opacity = "1";    
+            }, 1000);
+            bannerStatus = 1;
+        }
+    }
